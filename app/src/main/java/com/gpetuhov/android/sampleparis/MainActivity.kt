387fad29fd64fd8f95239a8c99c9b2bbc172eb52
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-09-04 10:44:58 +0300

    Set red text color style

--- app/src/main/java/com/gpetuhov/android/sampleparis/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/sampleparis/MainActivity.kt
@@ -2,11 +2,17 @@ package com.gpetuhov.android.sampleparis
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.airbnb.paris.Paris
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        helloText.setOnClickListener {
+            Paris.style(helloText).apply(R.style.RedTextStyle)
+        }
     }
 }
